b26e2f190934dc89a0fce1c371965e1da256619c
isaac celis <Isaac.celis.3c@gmail.com>
2025-01-22 00:16:25 -0600

    Update identidad gráfica

--- resources/views/invitacion.blade.php
+++ resources/views/invitacion.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid"  background="{{asset('img/Fondo2.jpg')}}">
+<div class="elementos-centrados">
     <div class="padding div">
     <h1>ENVIAR ENCUESTA POR INTERNET</h1>
     <h1> {{$Egresado->nombre}} {{$Egresado->paterno}} {{$Egresado->materno}}</h1>
--- resources/views/muestras/index.blade.php
+++ resources/views/muestras/index.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid"  background="{{asset('img/Fondo2.jpg')}}">
-    <div class="padding div" >
+<div class="container-fluid">
+  <div class="padding div">
     <h1>Hola  {{Auth::user()->name }} {{Auth::user()->emojis }}</h1>
-        <h1> Estas son tus muestras:</h1>
-    </div>
-    <center >
+    <h1> Estas son tus muestras:</h1>
+  </div>
+  <center >
   <a href="{{route('muestras20.plantel_index')}}">
-    <button class='boton-muestras' >
-       <br><br>ENCUESTA DE SEGUIMIENTO 2020 <br><br><br>
-    </button></a>
+  <button class='boton-muestras' >
+      <br><br>ENCUESTA DE SEGUIMIENTO 2020 <br><br><br>
+  </button></a>
   <a href="{{route('muestras14.index')}}">
     <button class='boton-muestras' >
-       <br><br>ENCUESTA DE ACTUALIZACION 2014 <br><br><br>
-    </button></a>
+      <br><br>ENCUESTA DE ACTUALIZACION 2014 <br><br><br>
+    </button>
+  </a>
   </center>
-
-
-    </div>
+</div>
     @endsection
 
     @push('css')
