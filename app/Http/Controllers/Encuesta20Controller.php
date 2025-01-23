94043a92637d32bb574eb33a88ef8e6aa4fff375
fgonzalez <felmiquiztli@gmail.com>
2025-01-23 15:36:45 -0600

    asf

--- app/Http/Controllers/Encuesta20Controller.php
+++ app/Http/Controllers/Encuesta20Controller.php
@@ -192,8 +192,8 @@ class Encuesta20Controller extends Controller
 
     function render($id,$section){
           $Encuesta=respeustas20::find($id);
-          $Reactivos=Reactivos::where('section',$section).>get();
-          return view('encuesta.render20',compact('Reactivos'))
+          $Reactivos=Reactivos::where('section',$section)->get();
+          return view('encuesta.render20',compact('Reactivos'));
 
     }
 
