9f190376b564b36a521eec7714bcff5bda1e191c
fgonzalez <felmiquiztli@gmail.com>
2025-03-13 12:39:03 -0600

    asf

--- resources/views/encuesta/show_16.blade.php
+++ resources/views/encuesta/show_16.blade.php
@@ -31,7 +31,7 @@ fata: {{session('falta')}}
             <br>
         @else
             <div class="react_container" id="{{'container'.$reactivo->clave}}" >
-            <h3>{{$reactivo->act_order}}.- @if($reactivo->act_description) {{$reactivo->act_description}} @else {{$reactivo->description}} @endif {{$reactivo->clave}}</h3>
+            <h3>{{$reactivo->act_order}}.- @if($reactivo->act_description) {{$reactivo->act_description}} @else {{$reactivo->description}} @endif {{--{{$reactivo->clave}}--}}</h3>
             @php $field_presenter=$reactivo->clave @endphp
             {{ComponentController::RenderReactive($reactivo,$opciones,$Encuesta->$field_presenter)}}
             </div>
