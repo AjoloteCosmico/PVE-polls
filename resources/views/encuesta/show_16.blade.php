a83fbba7edf1d3f8430c3d45d044cd6cb722e26c
fgonzalez <felmiquiztli@gmail.com>
2025-03-20 18:05:48 -0600

    sf

--- resources/views/encuesta/show_16.blade.php
+++ resources/views/encuesta/show_16.blade.php
@@ -32,7 +32,7 @@ use \App\Http\Controllers\ComponentController;
         @else
             <div class="react_container" id="{{'container'.$reactivo->clave}}" >
             
-            <h3>{{$reactivo->act_order}}.- @if($reactivo->act_description) {{$reactivo->act_description}} @else {{$reactivo->description}} @endif {{$reactivo->clave}}</h3>
+            <h3>{{$reactivo->act_order}}.- @if($reactivo->act_description) {{$reactivo->act_description}} @else {{$reactivo->description}} @endif {{--{{$reactivo->clave}}--}}</h3>
             @php $field_presenter=$reactivo->clave @endphp
             {{ComponentController::RenderReactive($reactivo,$opciones,$Encuesta->$field_presenter)}}
             </div>
