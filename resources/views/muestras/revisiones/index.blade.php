404e8c6f30ab8f2f64983058bcd403f89c362eb5
davo1956 <david1956@ciancias.unam.mx>
2025-03-25 13:03:40 -0600

    secciones en revisar encuestas y añadido act16

---
+++ resources/views/muestras/revisiones/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+  <div  class="padding div">
+    <h1>Hola  {{Auth::user()->name }} {{Auth::user()->emojis }}</h1>
+    <h1> Estas son tus revisiones:</h1>
+  </div>
+  <center >
+    <div style="display:flex; justify-content:space-around; align-content:space-around; align-items: flex-start; margin:4.9vh; padding:5.6vh;flex-wrap: wrap">
+  <a href="{{route('muestras.seg20.revision')}}">
+  <button class='boton-muestras' >
+      <br><br>ENCUESTA DE SEGUIMIENTO 2020 <br><br><br>
+  </button></a>
+  <a href="{{route('muestras.act16.revision')}}">
+    <button class='boton-muestras' >
+      <br><br>ENCUESTA DE ACTUALIZACION 2016 <br><br><br>
+    </button>
+  </a>
+  </div>
+  </center>
+</div>
+    @endsection
+
+    @push('css')
+    @endpush
