a95ceb5440c2208ceb244cf19f3c6f0d0d902d22
davo1956 <david1956@ciancias.unam.mx>
2026-01-22 12:08:52 -0600

    busqueda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -222,13 +222,15 @@ $egresados_posgrado = DB::table('egresados_posgrado')
             
             $join->on(DB::raw('CAST(codigos.code AS TEXT)'), '=', DB::raw('CAST(egresados_posgrado.status AS TEXT)'));
         })
-        ->leftJoin('respuestas20', function($join){
+        ->leftJoin('respuestas_posgrado', function($join){
             
-            $join->on(DB::raw('CAST(respuestas20.cuenta AS TEXT)'), '=', DB::raw('CAST(egresados_posgrado.cuenta AS TEXT)'));
+            $join->on(DB::raw('CAST(respuestas_posgrado.cuenta AS TEXT)'), '=', DB::raw('CAST(egresados_posgrado.cuenta AS TEXT)'));
         })
-        ->leftJoin('users as u_posgrado', 'u_posgrado.clave', '=', 'respuestas20.aplica')
+        ->leftJoin('users as u_posgrado', function($join){
+            $join->on(DB::raw('CAST(u_posgrado.clave AS TEXT)'), '=', DB::raw('CAST(respuestas_posgrado.aplica AS TEXT)'));
+        })
         ->select(
-            'egresados_posgrado.*', 'egresados_posgrado.cuenta as cuenta_posgrado', 'programa as programa_posgrado', 'plan as plan_posgrado', 'codigos.description as estado', 'codigos.color_rgb as color_codigo', 'respuestas20.updated_at as fecha_20', 'respuestas20.fec_capt as fechaFinal_20', 'respuestas20.completed as rpos20_completed', 'u_posgrado.name as aplicador_posgrado')
+            'egresados_posgrado.*', 'egresados_posgrado.cuenta as cuenta_posgrado', 'egresados_posgrado.programa as programa_posgrado', 'egresados_posgrado.plan as plan_posgrado', 'codigos.description as estado', 'codigos.color_rgb as color_codigo', 'respuestas_posgrado.updated_at as fecha_posgrado', 'respuestas_posgrado.fec_capt as fechaFinal_posgrado', 'respuestas_posgrado.completed as rpos20_completed', 'u_posgrado.name as aplicador_posgrado')
         ->where(DB::raw('CAST(egresados_posgrado.cuenta AS TEXT)'), 'LIKE', substr($request->nc, 0, 6) . '%')
         ->whereBetween('egresados_posgrado.anio_egreso', [2019, 2022])   
         ->get();
@@ -290,11 +292,13 @@ $egresados_posgrado = DB::table('egresados_posgrado')
             ->leftJoin('codigos', function($join){
                 $join->on(DB::raw('CAST(codigos.code AS TEXT)'), '=', DB::raw('CAST(egresados_posgrado.status AS TEXT)'));
             })
-            ->leftJoin('respuestas20', function($join){
-                $join->on(DB::raw('CAST(respuestas20.cuenta AS TEXT)'), '=', DB::raw('CAST(egresados_posgrado.cuenta AS TEXT)'));
+            ->leftJoin('respuestas_posgrado', function($join){
+                $join->on(DB::raw('CAST(respuestas_posgrado.cuenta AS TEXT)'), '=', DB::raw('CAST(egresados_posgrado.cuenta AS TEXT)'));
             })
-            ->leftJoin('users as u_posgrado', 'u_posgrado.clave', '=', 'respuestas20.aplica')
-            ->select('egresados_posgrado.*', 'egresados_posgrado.cuenta as cuenta_posgrado', 'programa as programa_posgrado', 'plan as plan_posgrado', 'codigos.description as estado', 'codigos.color_rgb as color_codigo', 'respuestas20.updated_at as fecha_20', 'respuestas20.fec_capt as fechaFinal_20', 'respuestas20.completed as rpos20_completed', 'u_posgrado.name as aplicador_posgrado')
+            ->leftJoin('users as u_posgrado', function($join){
+                $join->on(DB::raw('CAST(u_posgrado.clave AS TEXT)'), '=', DB::raw('CAST(respuestas_posgrado.aplica AS TEXT)'));
+            })
+            ->select('egresados_posgrado.*', 'egresados_posgrado.cuenta as cuenta_posgrado', 'egresados_posgrado.programa as programa_posgrado', 'egresados_posgrado.plan as plan_posgrado', 'codigos.description as estado', 'codigos.color_rgb as color_codigo', 'respuestas_posgrado.updated_at as fecha_posgrado', 'respuestas_posgrado.fec_capt as fechaFinal_posgrado', 'respuestas_posgrado.completed as rpos20_completed', 'u_posgrado.name as aplicador_posgrado')
             ->where(function($query) use ($partes_nombre) {
                 foreach ($partes_nombre as $parte) {
                     $query->where(function($subQuery) use ($parte) {
