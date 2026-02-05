032d37762c0961914aeb139ec8a2f3478b1e840e
davo1956 <david1956@ciancias.unam.mx>
2026-02-05 12:49:47 -0600

    if de fel

--- resources/views/posgrado/scripts_bloquear.blade.php
+++ resources/views/posgrado/scripts_bloquear.blade.php
@@ -18,107 +18,106 @@ array_bloqueos={
        },
        @endforeach
       };
+    // Función de bloqueo 
+function checkBloqueos(reactive, isInitialLoad = false) {
+    console.log('---CHANGE: ' + reactive + ' Ha cambiado');
 
-// Función de bloqueo
-    function checkBloqueos(reactive, isInitialLoad = false) {
-        console.log('---CHANGE: ' + reactive + ' Ha cambiado');
+    const elemento = document.getElementById(reactive);
+    let selected_values = [];
 
-        const elemento = document.getElementById(reactive);
-        let selected_values = [];
+    
+    if (elemento && elemento.tagName === 'SELECT') {
+        if (elemento.value) selected_values.push(elemento.value);
+    } else {
+        const checkboxes = document.querySelectorAll(`[id^="${reactive}-"]:checked`);
+        checkboxes.forEach(checkbox => {
+            selected_values.push(checkbox.value);
+        });
+    }
 
-        if (elemento && elemento.tagName === 'SELECT') {
-            const selected_value = elemento.value;
-            if (selected_value) {
-                selected_values.push(selected_value);
-            }
-        } 
-        else {
-            const checkboxes = document.querySelectorAll(`[id^="${reactive}-"]:checked`);
-            checkboxes.forEach(checkbox => {
-                selected_values.push(checkbox.value);
-            });
-        }   
+    // se usa para distiguir entre la carga inicial o un cambio posterior
+    if (!isInitialLoad) {
+        const involucrados = array_bloqueos[reactive]?.involucrados || [];
+        involucrados.forEach(involucrado => {
+            if (!involucrado) return; 
 
-        // se usa para distiguir entre la carga inicial o un cambio posterior
-        if (!isInitialLoad) {
-            const involucrados = array_bloqueos[reactive]?.involucrados || [];
-            involucrados.forEach(involucrado => {
-                const elemento_bloqueado = document.getElementById(involucrado);
-                const contenedor_bloqueado = document.getElementById('container' + involucrado);
-                if (elemento_bloqueado && contenedor_bloqueado){
-                    if (!contenedor_bloqueado.classList.contains('bloqueado_inicialmente')) {
+            const elemento_bloqueado = document.getElementById(involucrado);
+            const contenedor_bloqueado = document.getElementById('container' + involucrado);
+            
+            if (elemento_bloqueado && contenedor_bloqueado) {
+                if (!contenedor_bloqueado.classList.contains('bloqueado_inicialmente')) {
                     console.log('desbloqueando ' + involucrado);
-                    if (elemento_bloqueado.value === '0') {
-                        elemento_bloqueado.value = '';
-                    }
+                    if (elemento_bloqueado.value === '0') elemento_bloqueado.value = '';
+                    
                     contenedor_bloqueado.style.backgroundColor = 'transparent';
                     elemento_bloqueado.style.backgroundColor = 'white';
-                    
                     elemento_bloqueado.disabled = false;
-                    //si  el reactivo es un rating,
-                    radio_buttons=document.querySelectorAll(`#${involucrado} input[type="radio"]`);
-                    // console.log('Radio buttons a bloquear: ', radio_buttons);
+
+                    // Desbloquear radios/ratings
+                    let radio_buttons = document.querySelectorAll(`#${involucrado} input[type="radio"]`);
                     radio_buttons.forEach(radio => {
                         radio.disabled = false;
                         radio.style.backgroundColor = 'transparent';
                     });
-                    radio_button_divs=contenedor_bloqueado.querySelectorAll(`.rating-option-wrapper`);
-                    // console.log('Radio buttons a bloquear: ', radio_buttons);
+                    
+                    let radio_button_divs = contenedor_bloqueado.querySelectorAll(`.rating-option-wrapper`);
                     radio_button_divs.forEach(radio => {
                         radio.style.backgroundColor = 'transparent';
-                        //si entra a este ciclo, significa que es un contenedor de radio-buttons, donde elemento_bloqueado es un div, y no un input, asi que evitamos que se quede blanco
                         elemento_bloqueado.style.backgroundColor = 'transparent';
                     });
-                  }
-                
                 }
-            $('[id^="'+involucrado+'-"]').prop('disabled', false);      
-            });
-        }
+            }
+            $('[id^="' + involucrado + '-"]').prop('disabled', false);
+        });
+    }
 
-        // 2. Bloquear los reactivos basándose en los valores seleccionados
-        selected_values.forEach(selected_value => {
-            const reactivos_a_bloquear = array_bloqueos[reactive]?.[selected_value] || [];
+     // 2. Bloquear los reactivos basándose en los valores seleccionados
+    selected_values.forEach(selected_value => {
+        const reactivos_a_bloquear = array_bloqueos[reactive]?.[selected_value] || [];
 
-            if (reactivos_a_bloquear.length > 0) {
-                console.log('se bloquea por valor ' + selected_value + ': ', reactivos_a_bloquear);
+        if (reactivos_a_bloquear.length > 0) {
+            console.log('se bloquea por valor ' + selected_value + ': ', reactivos_a_bloquear);
 
-                reactivos_a_bloquear.forEach(bloqueado => {
-                    const elemento_bloqueado = document.getElementById(bloqueado);
-                    const contenedor_bloqueado = document.getElementById('container' + bloqueado);
-                    if (elemento_bloqueado && contenedor_bloqueado) {
-                        console.log('bloqueando ' + bloqueado);
-                        // No modificar el valor para no deshabilitar otros campos
-                        elemento_bloqueado.value = '0';
-                        contenedor_bloqueado.style.backgroundColor = '#252E56';
-                        elemento_bloqueado.style.backgroundColor = 'gray';
-                        elemento_bloqueado.disabled = true;
-                        //si  el reactivo es un rating,
-                    radio_buttons=document.querySelectorAll(`#${bloqueado} input[type="radio"]`);
-                    // console.log('Radio buttons a bloquear: ', radio_buttons);
+            reactivos_a_bloquear.forEach(bloqueado => {
+                if (!bloqueado) return; 
+
+                //IF DE FEL
+                if (selected_values.includes("1") && (bloqueado === 'edc5' || bloqueado === 'edc5otra')) {
+                    console.log('-> Ignorando bloqueo de ' + bloqueado + ' porque la opción 1 está activa');
+                    return; 
+                }
+
+                const elemento_bloqueado = document.getElementById(bloqueado);
+                const contenedor_bloqueado = document.getElementById('container' + bloqueado);
+                
+                if (elemento_bloqueado && contenedor_bloqueado) {
+                    console.log('bloqueando ' + bloqueado + ' por valor ' + selected_value);
+                    elemento_bloqueado.value = '0';
+                    contenedor_bloqueado.style.backgroundColor = '#252E56';
+                    elemento_bloqueado.style.backgroundColor = 'gray';
+                    elemento_bloqueado.disabled = true;
+                    //si  el reactivo es un rating,
+                    let radio_buttons = document.querySelectorAll(`#${bloqueado} input[type="radio"]`);
                     radio_buttons.forEach(radio => {
                         radio.checked = false;
                         radio.disabled = true;
                         radio.style.backgroundColor = '#252E56';
                     });
-                    radio_button_divs=contenedor_bloqueado.querySelectorAll(`.rating-option-wrapper`);
-                    // console.log('Radio buttons a bloquear: ', radio_buttons);
+                    
+                    let radio_button_divs = contenedor_bloqueado.querySelectorAll(`.rating-option-wrapper`);
                     radio_button_divs.forEach(radio => {
                         radio.style.backgroundColor = '#08090a';
                     });
-                    }
-                //si hay checkboxes
-                $('[id^="'+bloqueado+'-"]').prop('disabled', true);    
+                }
+                $('[id^="' + bloqueado + '-"]').prop('disabled', true);
+            });
+        }
+    });
 
-                });
-                
-            }
-        });
-
-        if (selected_values.length === 0) {
+    if (selected_values.length === 0) {
             console.log('Ninguna opción seleccionada, nada por cerrar.');
         }
-    }
+}
 
     //aplica estilo de bloquepo a las preguntas que venian bloqueadas
     document.addEventListener('DOMContentLoaded', (event) => {
