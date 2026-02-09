29989adcd74cbca379cc7807f34ae906a20734e2
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-02-09 12:37:29 -0600

    parchar recados conteo por tipo licenciatura

--- app/Http/Controllers/RecadosController.php
+++ app/Http/Controllers/RecadosController.php
@@ -107,6 +107,7 @@ class RecadosController extends Controller
       
       $Egresado->llamadas=$Recados=Recado::where('cuenta','=',$Egresado->cuenta)
       ->where('type','!=','cont')
+      ->orWhere('type',null)
       ->get()->count();
       $Egresado->save();
       if($Egresado->status!=1&&$Egresado->status!=2){
