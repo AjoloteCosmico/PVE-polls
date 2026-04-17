fb8119508e6cca31d7efb16174e658f8350889cb
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-04-17 09:54:50 -0600

    correccion a miestra

--- app/Http/Controllers/MuestrasController.php
+++ app/Http/Controllers/MuestrasController.php
@@ -691,7 +691,7 @@ public function show_posgrado($programa, $plan){
   
   $muestra = DB::table('egresados_posgrado')
     ->where('programa', '=', $programa)
-    // ->where('plan', '=', $plan)
+    ->where('plan', '=', $plan)
     ->whereIn('anio_egreso', ['2019', '2020', '2021', '2022'])
     ->where('fuente',  '=', 'base original')
     ->leftJoin('codigos',function($join){
