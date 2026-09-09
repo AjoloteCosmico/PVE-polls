f93260a400382b7d0e87af53df43a2c7a59a5f3f
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-09-09 14:16:29 -0600

    git push origin master

--- resources/views/mails/actualizacion.blade.php
+++ resources/views/mails/actualizacion.blade.php
@@ -8,16 +8,17 @@
 
 @section('content')
     <p style="margin: 0 0 12px; line-height: 1.8; color: #333;">
-        Estimado egresado
+      &nbsp;&nbsp;  Estimado egresado
         <span style="color: #B7812C; font-weight: 700;">{{ $payload['nombre'] }}</span>,
         con No. Cuenta:
         <span style="color: #B7812C; font-weight: 700;">{{ $payload['cuenta'] }}</span>.
     </p>
+<p>Hace algunos años compartiste con nosotros información sobre tu trayectoria como egresado de la UNAM. Hoy queremos volver a saber de ti y conocer cómo ha evolucionado tu experiencia profesional y académica.</p>
+<p>Por ello, te invitamos a contestar esta breve encuesta de actualización, en la que podrás compartir información sobre tu trayectoria profesional, actualización académica y percepción sobre tu formación en la licenciatura de <span style="color: #015190; font-weight: 800;">{{ $payload['prog_acad'] }}</span>.</p>
+<p>Tu participación nos permite conocer cómo se desarrollan profesionalmente nuestros egresados, identificar las áreas en las que están presentes y contar con información que contribuya a mejorar los planes de estudio y la atención a nuestra comunidad universitaria.</p>
+<p>Además, tu experiencia puede ser de gran utilidad para quienes están por elegir una licenciatura y desean conocer las oportunidades y caminos profesionales que pueden encontrar después de estudiar en la UNAM.</p>
+<p><span style="color: #B7812C; font-weight: 700;">Tu trayectoria cuenta. Ayúdanos a seguir conociendo a nuestros egresados.</span></p>
 
-    <p>Para nosotros es vital conocer tu trayectoria profesional, actualización académica y satisfacción con la institución respecto a la licenciatura <span style="color: #015190; font-weight: 800;">{{ $payload['prog_acad'] }}</span>.</p>
-    <p>Contestar esta breve encuesta nos permite perfilar a los egresados de las diferentes carreras de la UNAM; saber en qué áreas están presentes y cómo mejorar los planes de estudio y la atención a la comunidad.</p>
-    <p>También ayuda a los aspirantes a elegir correctamente la licenciatura que estudiarán; hazlo por todos, hazlo por tu universidad.</p>
-    <p>Este correo se envía a las generaciones que respondieron la encuesta de seguimiento a 3 años de egreso (generación de egreso 2018). Se realiza una actualización 5 años después para actualizar la trayectoria de los egresados.</p>
 
     <div style="text-align: center; margin: 30px 0;">
         <a href="https://encuestas.pveaju.unam.mx/pveaju/resource/encuesta_act_correo" style="background-color: #015190; color: #ffffff; padding: 15px 25px; text-decoration: none; border-radius: 5px; font-weight: bold;">
